898330623e9ca4617cb367e21fd60e75d6e1a130
Chidori <business.faissal@outlook.com>
2021-12-02 17:52:42 -0600

    minor additions to js

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,17 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
+var displayWarning = function(repo) {
+  // add text to warning container 
+  limitWarningEl.textContent = "To see more than 30 issues, visit ";
+  var linkEl = document.createElement("a");
+  linkEl.textContent = "See More Issues on Github.com";
+  linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+  linkEl.setAttribute("target", "_blank");
+
+  // append to warning container
+  limitWarningEl.appendChild(linkEl)
+}
 
 
 var displayIssues = function(issues) {
@@ -49,6 +61,11 @@ var getReposIssues = function (repo) {
       response.json().then(function(data) {
         // pass response data to dom function
         displayIssues(data);
+
+        // check i fapi has paginated issues
+        if (response.headers.get("Link")) {
+          displayWarning(repo);
+        }
       });
     } else {
       alert("There was a problem with your request!");
