b04defca032cec4fdcbabbc553c0ec86f50588b0
Chidori <business.faissal@outlook.com>
2021-12-02 18:42:14 -0600

    list issues completed

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,9 +1,50 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+  // grab repo name from url query string 
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+  
+  if (repoName) {
+    // display repo name on the page
+    repoNameEl.textContent = repoName;
+
+    getReposIssues(repoName);
+  } else {
+    //if no repo was given, redirect to the homepage
+    document.location.replace("./index.html");
+  }
+};
+
+var getReposIssues = function (repo) {
+  var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+
+  fetch(apiUrl).then(function(response) {
+    // request was successful
+    if (response.ok) {
+      response.json().then(function(data) {
+        // pass response data to dom function
+        displayIssues(data);
+
+        // check i fapi has paginated issues
+        if (response.headers.get("Link")) {
+          displayWarning(repo);
+        }
+      });
+    } else {
+      // if not successful, redirect to homepage
+      document.location.replace("./index.html");
+    }
+  });
+};
 
 var displayWarning = function(repo) {
   // add text to warning container 
   limitWarningEl.textContent = "To see more than 30 issues, visit ";
+  
+  // creates the link element
   var linkEl = document.createElement("a");
   linkEl.textContent = "See More Issues on Github.com";
   linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
@@ -11,7 +52,7 @@ var displayWarning = function(repo) {
 
   // append to warning container
   limitWarningEl.appendChild(linkEl)
-}
+};
 
 
 var displayIssues = function(issues) {
@@ -50,27 +91,4 @@ var displayIssues = function(issues) {
   }
 };
 
-
-
-var getReposIssues = function (repo) {
-  var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
-
-  fetch(apiUrl).then(function(response) {
-    // request was successful
-    if (response.ok) {
-      response.json().then(function(data) {
-        // pass response data to dom function
-        displayIssues(data);
-
-        // check i fapi has paginated issues
-        if (response.headers.get("Link")) {
-          displayWarning(repo);
-        }
-      });
-    } else {
-      alert("There was a problem with your request!");
-    }
-  });
-};
-
-getReposIssues("facebook/react");
+getRepoName();
